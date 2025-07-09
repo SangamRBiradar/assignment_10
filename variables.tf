d6b5e27513094d443ac77b258ed31693c66ecd91
SangamRBiradar <130237608+SangamRBiradar@users.noreply.github.com>
2025-07-09 18:45:48 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,7 +1,27 @@
-variable "resource_group_name" {}
-variable "location" {}
-variable "sql_admin_login" {}
-variable "sql_admin_password" {}
-variable "admin_username" {}
-variable "public_key" {}
-variable "subnet_id" {}
+variable "resource_group_name" {
+  default = "my-app-rg"
+}
+
+variable "location" {
+  default = "East US"
+}
+
+variable "sql_admin_login" {
+  default = "sqladmin"
+}
+
+variable "sql_admin_password" {
+  default = "StrongPassword123!"  # Consider using secrets for sensitive data
+}
+
+variable "admin_username" {
+  default = "azureuser"
+}
+
+variable "public_key" {
+  default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQD..."  # Replace with actual key
+}
+
+variable "subnet_id" {
+  default = "/subscriptions/xxxx/resourceGroups/my-app-rg/providers/Microsoft.Network/virtualNetworks/myVnet/subnets/default"
+}
