0eb4451bbd8cac1cb50f642f713b7d00313f645c
SangamRBiradar <130237608+SangamRBiradar@users.noreply.github.com>
2025-07-09 18:45:06 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,20 +1,7 @@
-variable "resource_group_name" {
-  default = "terraform-rg"
-}
-
-variable "location" {
-  default = "East US"
-}
-
-variable "sql_admin_user" {
-  default = "sqladmin"
-}
-
-variable "sql_admin_password" {
-  default = "StrongP@ssword123"
-}
-
-variable "database_name" {
-  default = "mydatabase"
-}
-
+variable "resource_group_name" {}
+variable "location" {}
+variable "sql_admin_login" {}
+variable "sql_admin_password" {}
+variable "admin_username" {}
+variable "public_key" {}
+variable "subnet_id" {}
