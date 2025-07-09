ad69a58c48a2eacd0b6ab0e4b0fba8846d359f50
SangamRBiradar <130237608+SangamRBiradar@users.noreply.github.com>
2025-07-09 18:41:18 +0530

    Update main.tf

--- modules/sql_database/main.tf
+++ modules/sql_database/main.tf
@@ -1,9 +1,6 @@
 resource "azurerm_mssql_database" "db" {
-  name       = var.database_name
-  server_id  = var.sql_server_id
-  sku_name   = "S0"
-}
-
-output "database_name" {
-  value = azurerm_mssql_database.db.name
+  name                = var.name
+  server_id           = var.sql_server_id
+  sku_name            = "S0"
+  collation           = "SQL_Latin1_General_CP1_CI_AS"
 }
