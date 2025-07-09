184b4446d9962f983ce0fa6d551456c59f35b41f
SangamRBiradar <130237608+SangamRBiradar@users.noreply.github.com>
2025-07-09 17:19:36 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -4,5 +4,6 @@ terraform {
     storage_account_name = "tfstatestorespp"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
+    use_azuread_auth     = true
   }
 }
