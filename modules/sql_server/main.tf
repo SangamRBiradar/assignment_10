1606b5e5b66127c90a15418ca6f162c1a63ecd78
SangamRBiradar <130237608+SangamRBiradar@users.noreply.github.com>
2025-07-09 18:40:19 +0530

    Update main.tf

--- modules/sql_server/main.tf
+++ modules/sql_server/main.tf
@@ -1,21 +1,8 @@
-resource "azurerm_mssql_server" "server" {
-  name                         = "sql-server-${random_pet.server.id}"
+resource "azurerm_mssql_server" "sql" {
+  name                         = var.name
   resource_group_name          = var.resource_group_name
   location                     = var.location
   version                      = "12.0"
-  administrator_login          = var.admin_user
+  administrator_login          = var.admin_login
   administrator_login_password = var.admin_password
 }
-
-resource "random_pet" "server" {
-  length = 2
-}
-
-output "sql_server_name" {
-  value = azurerm_mssql_server.server.name
-}
-
-output "sql_server_id" {
-  value = azurerm_mssql_server.server.id
-}
-
