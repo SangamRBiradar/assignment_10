d28c007b8dfdb3b5444e259464b56594a9564e54
SangamRBiradar <130237608+SangamRBiradar@users.noreply.github.com>
2025-07-09 18:46:15 +0530

    Update variables.tf

--- modules/linux_vm/variables.tf
+++ modules/linux_vm/variables.tf
@@ -1,6 +1,23 @@
-variable "vm_count" {}
-variable "resource_group_name" {}
-variable "location" {}
-variable "admin_username" {}
-variable "public_key" {}
-variable "subnet_id" {}
+variable "vm_count" {
+  default = 2
+}
+
+variable "resource_group_name" {
+  default = "my-app-rg"
+}
+
+variable "location" {
+  default = "East US"
+}
+
+variable "admin_username" {
+  default = "azureuser"
+}
+
+variable "public_key" {
+  default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQD..."
+}
+
+variable "subnet_id" {
+  default = "/subscriptions/xxxx/resourceGroups/my-app-rg/providers/Microsoft.Network/virtualNetworks/myVnet/subnets/default"
+}
